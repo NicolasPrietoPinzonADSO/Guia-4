033843c97cdb02b95ad48d798adb8d0760f1a308
NicolasPrietoPinzonADSO <nicolasprietopinzon5@gmail.com>
2022-09-28 21:18:38 -0500

    Ejercicio 3

--- main.js
+++ main.js
@@ -1,24 +1,20 @@
+
 addEventListener("DOMContentLoaded", () =>{
-    // 2. Hacer un algoritmo que al ingresar 2 números por la pantalla y se calcule la suma, resta, multiplicación
-    // y división. El proceso debe terminar cuando se hallan realizado 10 procesos (Hacer uso de
-    // contadores
+    // 3. Programa para solicitar el nombre, apellido, edad y la nota promedio de 5 estudiantes de un curso de
+    // computación
     
-    procesosTot = 0
-
-    while (procesosTot<10){
-        let num1 = Number(prompt('Ingrese el primer numero'))
-        let num2 = Number(prompt('Ingrese el segundo  numero'))
-        let suma = num1+num2
-        let resta = num1-num2
-        let multiplicacion = num1*num2
-        let division = num1/num2
-
-        alert(`El resultado de la suma de ${num1} + ${num2} es : ${suma}`);
-        alert(`El resultado de la resta de ${num1} - ${num2} es : ${resta}`);
-        alert(`El resultado de la multiplicacion de ${num1} * ${num2} es : ${multiplicacion}`);
-        alert(`El resultado de la suma de ${num1} / ${num2} es : ${division   }`);
-
-        procesosTot+=1
+    let obj = {}
+    cantidadEstu = 0
+    notaProm = 0
+    while(cantidadEstu<5    ){
+        nombre = prompt('Ingrese su nombre')
+        edad = prompt('Ingrese su edad')
+        nota = prompt('Ingrese su nota final')
+        obj[`${nombre} de ${edad} años de edad`] = `Su nota es ${nota}`
+        cantidadEstu+=1
+        notaProm+=nota
     }
-    
+    let promFinal = notaProm/5
+    obj[`La nota promedio del curso de ${cantidadEstu} es de`] = promFinal
+    console.log(obj)
 })
